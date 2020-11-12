807ebccb6a937816155db733883bfa67aac02236
Haowen <qindazhu@gmail.com>
2020-11-12 15:22:37 +0800

    first commit on recipes

---
+++ common.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+# Copyright 2019-2020 Mobvoi AI Lab, Beijing, China (author: Fangjun Kuang)
+# Apache 2.0
+
+from datetime import datetime
+import logging
+import numpy as np
+import torch
+
+
+def setup_logger(log_filename, log_level='info'):
+    now = datetime.now()
+    date_time = now.strftime('%Y-%m-%d-%H-%M-%S')
+    log_filename = '{}-{}'.format(log_filename, date_time)
+    formatter = '%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s'
+    if log_level == 'debug':
+        level = logging.DEBUG
+    elif log_level == 'info':
+        level = logging.INFO
+    elif log_level == 'warning':
+        level = logging.WARNING
+    logging.basicConfig(filename=log_filename,
+                        format=formatter,
+                        level=level,
+                        filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(level)
+    console.setFormatter(logging.Formatter(formatter))
+    logging.getLogger('').addHandler(console)
+
+
+def load_checkpoint(filename, model):
+    logging.info('load checkpoint from {}'.format(filename))
+
+    checkpoint = torch.load(filename, map_location='cpu')
+
+    keys = ['state_dict', 'epoch', 'learning_rate', 'objf']
+    for k in keys:
+        assert k in checkpoint
+
+    if not list(model.state_dict().keys())[0].startswith('module.') \
+            and list(checkpoint['state_dict'])[0].startswith('module.'):
+        # the checkpoint was saved by DDP
+        logging.info('load checkpoint from DDP')
+        dst_state_dict = model.state_dict()
+        src_state_dict = checkpoint['state_dict']
+        for key in dst_state_dict.keys():
+            src_key = '{}.{}'.format('module', key)
+            dst_state_dict[key] = src_state_dict.pop(src_key)
+        assert len(src_state_dict) == 0
+        model.load_state_dict(dst_state_dict)
+    else:
+        model.load_state_dict(checkpoint['state_dict'])
+
+    epoch = checkpoint['epoch']
+    learning_rate = checkpoint['learning_rate']
+    objf = checkpoint['objf']
+
+    return epoch, learning_rate, objf
+
+
+def save_checkpoint(filename, model, epoch, learning_rate, objf, local_rank=0):
+    if local_rank != None and local_rank != 0:
+        return
+
+    logging.info('Save checkpoint to {filename}: epoch={epoch}, '
+                 'learning_rate={learning_rate}, objf={objf}'.format(
+                     filename=filename,
+                     epoch=epoch,
+                     learning_rate=learning_rate,
+                     objf=objf))
+    checkpoint = {
+        'state_dict': model.state_dict(),
+        'epoch': epoch,
+        'learning_rate': learning_rate,
+        'objf': objf
+    }
+    torch.save(checkpoint, filename)
+
+
+def save_training_info(filename,
+                       model_path,
+                       current_epoch,
+                       learning_rate,
+                       objf,
+                       best_objf,
+                       best_epoch,
+                       local_rank=0):
+    if local_rank != None and local_rank != 0:
+        return
+
+    with open(filename, 'w') as f:
+        f.write('model_path: {}\n'.format(model_path))
+        f.write('epoch: {}\n'.format(current_epoch))
+        f.write('learning rate: {}\n'.format(learning_rate))
+        f.write('objf: {}\n'.format(objf))
+        f.write('best objf: {}\n'.format(best_objf))
+        f.write('best epoch: {}\n'.format(best_epoch))
+
+    logging.info('write training info to {}'.format(filename))
---
+++ prepare.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+
+import torch
+
+from lhotse import CutSet, Fbank, Mfcc, LilcomFilesWriter, WavAugmenter
+from lhotse.dataset import SpeechRecognitionDataset
+from lhotse.recipes.librispeech import download_and_untar, prepare_librispeech, dataset_parts_full
+
+print(dataset_parts_full)
+
+dataset_parts = ('dev-clean', 'test-clean', 'train-clean-100')
+
+corpus_dir = '/home/storage04/zhuangweiji/data/open-source-data/librispeech/LibriSpeech'
+output_dir = 'exp/data1'
+librispeech_manifests = prepare_librispeech(corpus_dir, dataset_parts,
+                                            output_dir)
+
+use_data_augmentation = False
+augmenter = WavAugmenter.create_predefined(
+    'pitch_reverb_tdrop',
+    sampling_rate=16000) if use_data_augmentation else None
+# It seems when spawning multiple Python subprocesses with the same sox handle it raises "std::runtime_error: Couldn't close file"
+# The issue seems to happen only in a Jupyter notebook on Mac OS X, hence the work around below.
+if use_data_augmentation:
+    num_jobs = 1
+else:
+    num_jobs = os.cpu_count()
+    torch.set_num_threads(1)
+    torch.set_num_interop_threads(1)
+
+num_jobs = 1
+for partition, manifests in librispeech_manifests.items():
+    print(partition)
+    with LilcomFilesWriter(f'{output_dir}/feats_{partition}'
+                           ) as storage, ProcessPoolExecutor(num_jobs) as ex:
+        cut_set = CutSet.from_manifests(
+            recordings=manifests['recordings'],
+            supervisions=manifests['supervisions']).compute_and_store_features(
+                extractor=Fbank(),
+                storage=storage,
+                augmenter=augmenter if 'train' in partition else None,
+                executor=ex)
+    librispeech_manifests[partition]['cuts'] = cut_set
+    cut_set.to_json(output_dir + f'/cuts_{partition}.json.gz')
+
+cuts_train = SpeechRecognitionDataset(
+    librispeech_manifests['train-clean-100']['cuts'])
+cuts_test = SpeechRecognitionDataset(
+    librispeech_manifests['test-clean']['cuts'])
+
+sample = cuts_train[0]
+print('Transcript:', sample['text'])
+print('Supervisions mask:', sample['supervisions_mask'])
+print('Feature matrix:')
