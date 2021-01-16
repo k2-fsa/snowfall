Fix log-softmax and model output shapes

snowfall/models/tdnnf.py
@@ -131,11 +131,12 @@ class Tdnnf1a(AcousticModel):
         nnet_output = nnet_output.permute(0, 2, 1)
         # at this point, nnet_output is [N, T, C]
         nnet_output = self.output_affine(nnet_output)
+        nnet_output = F.log_softmax(nnet_output, dim=2)
         #print('After output_affine', nnet_output.shape)
         # we return nnet_output [N, C, T]
         nnet_output = nnet_output.permute(0, 2, 1)
         #print('Returning', nnet_output.shape)
-        return nnet_output.permute(0, 2, 1)
+        return nnet_output
 
 
 def constrain_orthonormal_hook(model, unused_x):