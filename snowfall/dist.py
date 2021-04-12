fc5b44586a30b97ca18a395676ec440ec23c05d5
Daniel Povey <dpovey@gmail.com>
2021-04-12 13:51:23 +0800

    Revert "Make master port configurable"

--- snowfall/dist.py
+++ snowfall/dist.py
@@ -3,14 +3,9 @@ import torch
 from torch import distributed as dist
 
 
-def setup_dist(rank, world_size, master_port = None):
+def setup_dist(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-<<<<<<< HEAD
     os.environ['MASTER_PORT'] = '12355'
-=======
-    os.environ['MASTER_PORT'] = ('12354' if master_port is None
-                                 else str(master_port))
->>>>>>> 8f51e68... Make master port command-line configurable
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
