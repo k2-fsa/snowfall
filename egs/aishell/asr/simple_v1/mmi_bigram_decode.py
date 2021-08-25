03a7e188d57e361ccb14436d7c55f7097bf4d24d
Mingshuang Luo <37799481+luomingshuang@users.noreply.github.com>
2021-08-25 09:27:09 +0800

    Update mmi_bigram_decode.py

--- egs/aishell/asr/simple_v1/mmi_bigram_decode.py
+++ egs/aishell/asr/simple_v1/mmi_bigram_decode.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright (c)  2020  Xiaomi Corporation (authors: Daniel Povey, Haowen Qiu, Mingshuang Luo)
+#                                           2021 Pingfeng Luo
 # Apache 2.0
 
 import k2
